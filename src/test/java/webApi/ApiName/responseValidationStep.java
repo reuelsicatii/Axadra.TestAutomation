5320ed192e5c9c001ec3543ef9523dc097fb1c75
Reuel Sicat II <reuel@truelogic.com.ph>
2023-09-27 15:50:56 +0800

    changes for taa-997

--- src/test/java/webApi/ApiName/responseValidationStep.java
+++ src/test/java/webApi/ApiName/responseValidationStep.java
@@ -1,20 +1,19 @@
 package webApi.ApiName;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.everit.json.schema.Schema;
-import org.everit.json.schema.ValidationException;
-import org.everit.json.schema.loader.SchemaLoader;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
 import com.aventstack.extentreports.GherkinKeyword;
 import com.aventstack.extentreports.Status;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
@@ -96,20 +95,23 @@ public class responseValidationStep {
 					JsonObject.class);
 			expectedPrettyJsonRespBody = gson.toJson(expectedJsonRespBody);
 
-			// Load the JSON schema from the file
-			try (FileReader expectedSchemaReader = new FileReader(
-					System.getProperty("user.dir") + expectedSchemaBody)) {
+			// Parse JSON schema and data using Jackson ObjectMapper
+			ObjectMapper objectMapper = new ObjectMapper();
+			JsonNode schemaNode = objectMapper.readTree(expectedPrettyJsonRespBody);
+			JsonNode dataNode = objectMapper.readTree(actualPrettyJsonRespBody);
 
-				JSONObject expectedRawSchema = new JSONObject(new JSONTokener(expectedSchemaReader));
+			// Create a JsonSchemaFactory
+			JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
 
-				// Create a Schema from the loaded JSON schema
-				Schema expectedSchema = SchemaLoader.load(expectedRawSchema);
+			// Compile the schema
+			JsonSchema schema = factory.getJsonSchema(schemaNode);
 
-				// Parse the JSON response
-				JSONObject actualJSONObject = new JSONObject(context.getResponseBody());
+		
+			
+			
 
-				// Perform schema validation
-				expectedSchema.validate(actualJSONObject);
+			// Check if validation passed
+			if (schema.validInstance(dataNode)) {
 
 				// Extent Report
 				context.getExtentTestScenario()
@@ -126,23 +128,72 @@ public class responseValidationStep {
 								+ "            <pre>" + actualPrettyJsonRespBody + "</pre>\r\n" + "        </div>\r\n"
 								+ "</div>");
 
-			} catch (ValidationException e) {
-
+			} else {
+				
 				// Extent Report
 				context.getExtentTestScenario()
 						.createNode(new GherkinKeyword("Given"),
 								"I validate response body against schema " + expectedSchemaBody)
-						.fail("FAILED: " + "<br>" + e.getMessage() + "<br>" + "<div style=\"display: flex\">\r\n"
-								+ "        <div style=\"flex: 1;\r\n" + "            border: 1px solid #ccc;\r\n"
-								+ "            padding: 20px;\r\n" + "            background-color: #f0f0f0;\">\r\n"
+						.fail("FAILED" 								
+								+ "<div style=\"display: flex;\">\r\n" + "        <div style=\"flex: 1;\r\n"
+								+ "            border: 1px solid #ccc;\r\n" + "            padding: 20px;\r\n"
+								+ "            background-color: #f0f0f0;\r\n" + "            max-width: 50%;\">\r\n"
 								+ "            <h2>Expected Result</h2>\r\n" + "            <pre>"
 								+ expectedPrettyJsonRespBody + "</pre>\r\n" + "        </div>\r\n"
 								+ "        <div style=\"flex: 1;\r\n" + "            border: 1px solid #ccc;\r\n"
-								+ "            padding: 20px;\r\n" + "            background-color: #e0e0e0;\">\r\n"
-								+ "            <h2>Actual Result</h2>\r\n" + "            <pre>"
-								+ actualPrettyJsonRespBody + "</pre>\r\n" + "        </div>\r\n" + "</div>");
-
+								+ "            padding: 20px;\r\n" + "            background-color: #e0e0e0;\r\n"
+								+ "            max-width: 50%;\">\r\n" + "            <h2>Actual Result</h2>\r\n"
+								+ "            <pre>" + actualPrettyJsonRespBody + "</pre>\r\n" + "        </div>\r\n"
+								+ "</div>");
 			}
+
+//			// Load the JSON schema from the file
+//			try (FileReader expectedSchemaReader = new FileReader(
+//					System.getProperty("user.dir") + expectedSchemaBody)) {
+//
+//				JSONObject expectedRawSchema = new JSONObject(new JSONTokener(expectedSchemaReader));
+//
+//				// Create a Schema from the loaded JSON schema
+//				Schema expectedSchema = SchemaLoader.load(expectedRawSchema);
+//
+//				// Parse the JSON response
+//				JSONObject actualJSONObject = new JSONObject(context.getResponseBody());
+//
+//				// Perform schema validation
+//				expectedSchema.validate(actualJSONObject);
+//
+//				// Extent Report
+//				context.getExtentTestScenario()
+//						.createNode(new GherkinKeyword("Given"),
+//								"I validate response body against schema " + expectedSchemaBody)
+//						.pass("PASSED" + "<div style=\"display: flex;\">\r\n" + "        <div style=\"flex: 1;\r\n"
+//								+ "            border: 1px solid #ccc;\r\n" + "            padding: 20px;\r\n"
+//								+ "            background-color: #f0f0f0;\r\n" + "            max-width: 50%;\">\r\n"
+//								+ "            <h2>Expected Result</h2>\r\n" + "            <pre>"
+//								+ expectedPrettyJsonRespBody + "</pre>\r\n" + "        </div>\r\n"
+//								+ "        <div style=\"flex: 1;\r\n" + "            border: 1px solid #ccc;\r\n"
+//								+ "            padding: 20px;\r\n" + "            background-color: #e0e0e0;\r\n"
+//								+ "            max-width: 50%;\">\r\n" + "            <h2>Actual Result</h2>\r\n"
+//								+ "            <pre>" + actualPrettyJsonRespBody + "</pre>\r\n" + "        </div>\r\n"
+//								+ "</div>");
+//
+//			} catch (ValidationException e) {
+//
+//				// Extent Report
+//				context.getExtentTestScenario()
+//						.createNode(new GherkinKeyword("Given"),
+//								"I validate response body against schema " + expectedSchemaBody)
+//						.fail("FAILED: " + "<br>" + e.getMessage() + "<br>" + "<div style=\"display: flex\">\r\n"
+//								+ "        <div style=\"flex: 1;\r\n" + "            border: 1px solid #ccc;\r\n"
+//								+ "            padding: 20px;\r\n" + "            background-color: #f0f0f0;\">\r\n"
+//								+ "            <h2>Expected Result</h2>\r\n" + "            <pre>"
+//								+ expectedPrettyJsonRespBody + "</pre>\r\n" + "        </div>\r\n"
+//								+ "        <div style=\"flex: 1;\r\n" + "            border: 1px solid #ccc;\r\n"
+//								+ "            padding: 20px;\r\n" + "            background-color: #e0e0e0;\">\r\n"
+//								+ "            <h2>Actual Result</h2>\r\n" + "            <pre>"
+//								+ actualPrettyJsonRespBody + "</pre>\r\n" + "        </div>\r\n" + "</div>");
+//
+//			}
 		} catch (Exception e) {
 
 			try {
@@ -180,14 +231,14 @@ public class responseValidationStep {
 			JsonObject actualJsonRespBody = gson.fromJson(context.getResponseBody(), JsonObject.class);
 			actualPrettyJsonRespBody = gson.toJson(actualJsonRespBody);
 
-			// Actual JSON
-			JSONObject actualJSONObject = new JSONObject(context.getResponseBody());
-
 			// Expected Pretty JSON
 			JsonObject expectedJsonRespBody = gson
 					.fromJson(new String(Files.readAllBytes(Paths.get(System.getProperty("user.dir") + expectedBody)),
 							StandardCharsets.UTF_8), JsonObject.class);
 			expectedPrettyJsonRespBody = gson.toJson(expectedJsonRespBody);
+
+			// Actual JSON
+			JSONObject actualJSONObject = new JSONObject(context.getResponseBody());
 
 			// Expected JSON
 			JSONObject expectedJSONObject = new JSONObject(
