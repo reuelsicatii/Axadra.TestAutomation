51ba82c8ac9acc54d3efa5f89a00e0abbdee0ab2
Reuel Sicat II <reuel@truelogic.com.ph>
2023-11-22 09:30:18 +0800

    changes for taa-1039

--- src/test/java/webApi/SEM/addParameterStep.java
+++ src/test/java/webApi/SEM/addParameterStep.java
@@ -93,106 +93,107 @@ public class addParameterStep {
 	public void iUpdateTheRequestBodyWhoseFieldIs(String requestBodyPath, String requestBodyField,
 			String requestBodyValue) throws Throwable {
 
-//		try {
+		try {
 
-		System.err.println("iUpdateTheRequestBodyWhoseFieldIs -- 0");
+			System.err.println("iUpdateTheRequestBodyWhoseFieldIs -- 0");
 
-		// Read the existing JSON file
-		// ==========================================
-		ObjectMapper objectMapper = new ObjectMapper();
-		JsonNode rootNode = objectMapper.readTree(new File(System.getProperty("user.dir") + requestBodyPath));
+			// Read the existing JSON file
+			// ==========================================
+			ObjectMapper objectMapper = new ObjectMapper();
+			JsonNode rootNode = objectMapper.readTree(new File(System.getProperty("user.dir") + requestBodyPath));
 
-		// Update the value of a nested property
-		// ==============================================================
-		// Split the property path into individual property names
-		String[] propertyNames = requestBodyField.split("\\.");
+			// Update the value of a nested property
+			// ==============================================================
+			// Split the property path into individual property names
+			String[] propertyNames = requestBodyField.split("\\.");
 
-		// Traverse the JSON structure to find the nested property
-		JsonNode currentNode = rootNode;
+			// Traverse the JSON structure to find the nested property
+			JsonNode currentNode = rootNode;
 
-		for (int i = 0; i < propertyNames.length; i++) {
+			for (int i = 0; i < propertyNames.length; i++) {
 
-			if (currentNode.has(propertyNames[i]) && currentNode.get(propertyNames[i]).isArray()) {
-				JsonNode ArrayNode = currentNode.get(propertyNames[i]);
+				if (currentNode.has(propertyNames[i]) && currentNode.get(propertyNames[i]).isArray()) {
+					JsonNode ArrayNode = currentNode.get(propertyNames[i]);
 
-				for (JsonNode indexArrayNode : ArrayNode) {
+					for (JsonNode indexArrayNode : ArrayNode) {
 
-					if (indexArrayNode.has(propertyNames[i + 1])) {
+						if (indexArrayNode.has(propertyNames[i + 1])) {
 
-						if (requestBodyValue.equals("dynamicURL")) {
+							if (requestBodyValue.equals("dynamicURL")) {
 
-							((ObjectNode) indexArrayNode).put(propertyNames[i + 1],
-									"https://www." + commonService.generateRandomString(6) + ".com/"
-											+ commonService.generateRandomString(10));
+								((ObjectNode) indexArrayNode).put(propertyNames[i + 1],
+										"https://www." + commonService.generateRandomString(6) + ".com/"
+												+ commonService.generateRandomString(10));
 
-						} else {
+							} else {
 
-							((ObjectNode) indexArrayNode).put(propertyNames[i + 1], requestBodyValue);
+								((ObjectNode) indexArrayNode).put(propertyNames[i + 1], requestBodyValue);
+
+							}
 
 						}
 
+						else {
+							System.out.println("Property field not found in links array element.");
+							break;
+						}
 					}
 
-					else {
-						System.out.println("Property field not found in links array element.");
-						break;
+					break;
+				}
+
+				else if (currentNode.has(propertyNames[i])) {
+
+					if (requestBodyValue.equals("dynamicURL")) {
+
+						System.err.println("iUpdateTheRequestBodyWhoseFieldIs -- 23");
+						((ObjectNode) currentNode).put(propertyNames[i],
+								"https://www." + commonService.generateRandomString(6) + ".com/"
+										+ commonService.generateRandomString(10));
+
+					} else {
+
+						System.err.println("iUpdateTheRequestBodyWhoseFieldIs -- 24");
+						((ObjectNode) currentNode).put(propertyNames[i], requestBodyValue);
+
 					}
+
+					break;
 				}
 
-				break;
-			}
-
-			else if (currentNode.has(propertyNames[i])) {
-
-				if (requestBodyValue.equals("dynamicURL")) {
-
-					System.err.println("iUpdateTheRequestBodyWhoseFieldIs -- 23");
-					((ObjectNode) currentNode).put(propertyNames[i], "https://www."
-							+ commonService.generateRandomString(6) + ".com/" + commonService.generateRandomString(10));
-
-				} else {
-
-					System.err.println("iUpdateTheRequestBodyWhoseFieldIs -- 24");
-					((ObjectNode) currentNode).put(propertyNames[i], requestBodyValue);
-
+				else {
+					// Handle the case where the property is missing
+					System.out.println("Property not found: " + propertyNames[i]);
+					break;
 				}
 
-				break;
 			}
 
-			else {
-				// Handle the case where the property is missing
-				System.out.println("Property not found: " + propertyNames[i]);
-				break;
-			}
+			System.err.println("iUpdateTheRequestBodyWhoseFieldIs  -- 3");
+			// Write the updated JSON data back to the file
+			// =====================================================================
+			objectMapper.writeValue(new File(System.getProperty("user.dir") + requestBodyPath), currentNode);
 
+			System.err.println("iUpdateTheRequestBodyWhoseFieldIs  -- 4");
+
+			// Extent Report
+			context.getExtentTestScenario().createNode(new GherkinKeyword("Given"),
+					"I update the " + requestBodyPath + " whose " + requestBodyField + " field is " + requestBodyValue)
+					.pass("PASSED");
+
+		} catch (Exception e) {
+
+			try {
+				// Extent Report
+				context.getExtentTestScenario().createNode(new GherkinKeyword("Given"), "I update the "
+						+ requestBodyPath + " whose " + requestBodyField + " field is " + requestBodyValue)
+						.fail("FAILED: " + e.getMessage());
+				context.getExtentTestScenario().log(Status.FAIL, "Failed");
+			} catch (ClassNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		}
-
-		System.err.println("iUpdateTheRequestBodyWhoseFieldIs  -- 3");
-		// Write the updated JSON data back to the file
-		// =====================================================================
-		objectMapper.writeValue(new File(System.getProperty("user.dir") + requestBodyPath), currentNode);
-
-		System.err.println("iUpdateTheRequestBodyWhoseFieldIs  -- 4");
-
-		// Extent Report
-		context.getExtentTestScenario().createNode(new GherkinKeyword("Given"),
-				"I update the " + requestBodyPath + " whose " + requestBodyField + " field is " + requestBodyValue)
-				.pass("PASSED");
-
-//		} catch (Exception e) {
-//
-//			try {
-//				// Extent Report
-//				context.getExtentTestScenario().createNode(new GherkinKeyword("Given"), "I update the "
-//						+ requestBodyPath + " whose " + requestBodyField + " field is " + requestBodyValue)
-//						.fail("FAILED: " + e.getMessage());
-//				context.getExtentTestScenario().log(Status.FAIL, "Failed");
-//			} catch (ClassNotFoundException e1) {
-//				// TODO Auto-generated catch block
-//				e1.printStackTrace();
-//			}
-//		}
 	}
 
 	@And("I add the {string} to the request")
