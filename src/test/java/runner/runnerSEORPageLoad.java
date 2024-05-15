7b25ed89d95d2b613d09e66768cfbc6ff5c83e50
Reuel Sicat II <reuel@axadra.com>
2024-05-15 08:36:52 +0800

    changes for taa-1141

---
+++ src/test/java/runner/runnerSEORPageLoad.java
@@ -0,0 +1,42 @@
+package runner;
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.CucumberOptions.SnippetType;
+
+
+
+
+@CucumberOptions(
+		
+			
+		features = {"featurefiles/webApp/seorPageLoad.feature"},
+		dryRun = !true,
+		snippets = SnippetType.CAMELCASE,
+		monochrome = true,				
+		glue = { "webApp.SEOR"},
+		tags =  "@SEOR_Page_And_Lazy_Load",
+		plugin =  {				
+				"pretty",
+				"html:reports/results.html",
+				"json:reports/results.json",
+				"junit:reports/results.xml"
+		}
+		
+
+	)
+
+public class runnerSEORPageLoad extends AbstractTestNGCucumberTests {	
+	
+	
+	// Running Cucumber - Parallel Test
+	// =========================================
+	@Override
+	@DataProvider (parallel = true)
+	public Object[][] scenarios() {
+		return super.scenarios();
+	}
+
+}
